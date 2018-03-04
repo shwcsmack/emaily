c2bcefc5e19d8a7bea5790a50572d524977d4e64
Shayne Stewart <shayne@code3dev.com>
2018-03-04 11:52:17 -0600

    Test Commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'buddy'});
 });
 
+//https://powerful-hollows-50447.herokuapp.com/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
