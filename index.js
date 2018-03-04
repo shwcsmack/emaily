26cecfaf37e312a3beced36c72ef6a04619ecbd0
Shayne Stewart <sh.wc.smack@gmail.com>
2018-03-04 12:08:38 -0600

    cleanup

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.get('/', (req, res) => {
     res.send({bye: 'buddy'});
 });
 
-//testing cross platform
-//one more
 //https://powerful-hollows-50447.herokuapp.com/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
